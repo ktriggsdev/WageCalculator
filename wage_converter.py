80c81864d3e367890f27e447e4f3c154c042d7f1
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-09-19 12:54:21 +0100

    edited docstring

--- wage_converter.py
+++ wage_converter.py
@@ -1,11 +1,12 @@
 docstring = """
 Hello, This is a program to calculate your weekly, monthly and annual wages based on the hourly wage you have.
 To start, please enter your name
-Then enter your hourly wage without the $ or £ symbols and in price format.
 
 Next enter your starting wage type: hourly (h), weekly (w), monthly (m) or annually (a)
 
 Then enter whether you wish to use Dollars (d) or Pounds (p)
+
+Finally enter your wage without the $ or £ symbols and in price format.
 
 The program will then calculate your weekly, monthly and annual wages
 
